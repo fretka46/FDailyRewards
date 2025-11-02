3d153d0f18b98b6a88d1f5997fd8ee5bb69abee2
fretka46 <fretka46.02@gmail.com>
2025-11-02 09:59:33 +0100

    Fix login issues

--- src/main/java/com/fretka46/fDailyRewards/Listeners/LoginListener.java
+++ src/main/java/com/fretka46/fDailyRewards/Listeners/LoginListener.java
@@ -1,7 +1,9 @@
 package com.fretka46.fDailyRewards.Listeners;
 
 import com.fretka46.fDailyRewards.FDailyRewards;
+import com.fretka46.fDailyRewards.Storage.ConfigManager;
 import com.fretka46.fDailyRewards.Storage.DatabaseManager;
+import com.fretka46.fDailyRewards.Utils.Log;
 import com.fretka46.fDailyRewards.Utils.Messages;
 import com.fretka46.fDailyRewards.Utils.Scheduler;
 import org.bukkit.event.EventHandler;
@@ -17,14 +19,8 @@ public class LoginListener implements Listener {
         var localtime = java.time.LocalDateTime.now();
         var config = FDailyRewards.getPlugin(FDailyRewards.class).getConfig();
 
-        if (DatabaseManager.hasLoggedInToday(player.getUniqueId(), localtime))
-            return;
-
-        if (config.getBoolean("show_reward_claim_available_message"))
-            Scheduler.scheduleRewardMessage(player);
-
         // Check player daily rewards
-        if (player.hasPermission("survival.login.tokeny"))
+        if (player.hasPermission("survival.login.tokeny") && !DatabaseManager.hasLoggedInToday(player.getUniqueId(), localtime))
         {
             // Set daily login and reward the player
             DatabaseManager.setLoggedInToday(player.getUniqueId(), localtime);
@@ -40,6 +36,30 @@ public class LoginListener implements Listener {
 
             // Send message
             Messages.sendTranslatedMessageTo(player, "login_rewarded");
+        }
+
+        if (config.getBoolean("show_reward_claim_available_message")) {
+            if (!DatabaseManager.hasClaimedRewardInLastDay(player.getUniqueId(), localtime)) {
+
+                // Check if player current day reward is vip and player is not vip
+                var isVip = player.hasPermission("survival.premium.dailylogin");
+                var nextDayClaim = DatabaseManager.getNextDayToClaim(player.getUniqueId(), !isVip);
+
+                // safety: ensure index is valid
+                if (nextDayClaim <= 0) {
+                    Log.info("Invalid nextDayClaim for player " + player.getName() + ": " + nextDayClaim);
+                    return;
+                }
+
+                var reward = ConfigManager.getRewardForDay(nextDayClaim - 1);
+
+                // pokud je odměna VIP a hráč není VIP, není dostupná -> nezobrazovat zprávu
+                if (reward != null && reward.vip && !isVip) {
+                    Log.info("Player " + player.getName() + " has a VIP reward available, but is not VIP. Not scheduling claim message.");
+                } else {
+                    Scheduler.scheduleRewardMessage(player);
+                }
+            }
         }
     }
 }
